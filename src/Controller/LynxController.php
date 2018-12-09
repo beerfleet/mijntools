10707dab79d4274e0861a9b08788261ff3c732fa
Jan Van Biervliet <jvbiervliet@gmail.com>
2018-12-09 12:20:21 +0100

    added post route

--- src/Controller/LynxController.php
+++ src/Controller/LynxController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class LynxController extends AbstractController {
 
@@ -10,6 +12,12 @@ class LynxController extends AbstractController {
     return $this->render('lynx/index.html.twig', [
           'controller_name' => 'LynxController',
     ]);
+  }
+  
+  public function process_add_link() {
+    $request = Request::createFromGlobals();
+    $link = $request->get('new-link', 'bestaat niet');
+    return new Response("<H1> $link </H1>");
   }
 
 }
