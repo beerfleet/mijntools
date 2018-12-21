208ae33d9bfe948020f0fc61f39af88e0c990fb6
jvanbiervliet <jan.van.biervliet@etion.be>
2018-12-21 12:22:17 +0100

    ORM: Added tags

---
+++ src/Migrations/Version20181221111448.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181221111448 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE users_groups (link_id INT NOT NULL, tag_id INT NOT NULL, INDEX IDX_FF8AB7E0ADA40271 (link_id), INDEX IDX_FF8AB7E0BAD26311 (tag_id), PRIMARY KEY(link_id, tag_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE users_groups ADD CONSTRAINT FK_FF8AB7E0ADA40271 FOREIGN KEY (link_id) REFERENCES link (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE users_groups ADD CONSTRAINT FK_FF8AB7E0BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE users_groups DROP FOREIGN KEY FK_FF8AB7E0BAD26311');
+        $this->addSql('DROP TABLE users_groups');
+        $this->addSql('DROP TABLE tag');
+    }
+}
